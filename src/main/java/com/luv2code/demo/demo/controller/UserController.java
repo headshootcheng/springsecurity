53524309343b54924e541aff7e097ced6d12aaa3
petercheng <petercheng7788@gmail.com>
2021-01-21 02:23:04 +0800

    Fixed Cors issue

--- src/main/java/com/luv2code/demo/demo/controller/UserController.java
+++ src/main/java/com/luv2code/demo/demo/controller/UserController.java
@@ -1,12 +1,10 @@
 package com.luv2code.demo.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
+//@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
 
     @GetMapping("/test")
